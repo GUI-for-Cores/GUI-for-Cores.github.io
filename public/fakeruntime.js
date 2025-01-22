e325adfdc0113b41e263e5c57393009aa9b2a625
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-01-22 23:11:26 +0800

    Add new methods to fakeruntime.js for improved app functionality and error handling

--- public/fakeruntime.js
+++ public/fakeruntime.js
@@ -3,15 +3,44 @@ window.runtime = {
   EventsOnMultiple(id, fn) {},
   WindowIsMaximised() {},
   WindowIsMinimised() {},
+  WindowReloadApp() {
+    window.location.reload();
+  },
+  WindowSetSize() {},
+  BrowserOpenURL(url) {
+    if (url.startsWith("http")) {
+      window.open(url, "_blank");
+      return;
+    }
+    url = url.replace(window.location.pathname, "");
+    const { data } = window.go.bridge.App.Readfile(url);
+    const link = URL.createObjectURL(
+      new Blob([data], { type: "text/plain; charset=utf-8" })
+    );
+    window.open(link, "_blank");
+  },
+  ClipboardSetText(msg) {
+    navigator.clipboard.writeText(msg);
+    return { flag: true };
+  },
 };
 
 window.go = {
   bridge: {
     App: {
+      RestartApp() {
+        Plugins.message.info("Web端不支持");
+      },
+      GetInterfaces() {
+        return { flag: true, data: "eth0|WLAN0" };
+      },
       UpdateTray() {},
       UpdateTrayMenus() {},
       GetEnv() {
-        return { os: "darwin" };
+        return {
+          os: "darwin",
+          basePath: window.location.pathname.slice(0, -1),
+        };
       },
       Readdir() {
         return { flag: true, data: "" };
@@ -47,8 +76,17 @@ window.go = {
         path = window.location.pathname + path;
         return { flag: true, data: localStorage.getItem(path) };
       },
+      Exec() {
+        return { flag: false, data: "Web端不支持" };
+      },
       ExecBackground(path, args, out, end) {
-        return { flag: true, data: 999 };
+        return { flag: false, data: "Web端不支持" };
+      },
+      ValidateCron() {
+        return { flag: true, data: "" };
+      },
+      AddScheduledTask() {
+        return { flag: false, data: "Web端不支持" };
       },
     },
   },
